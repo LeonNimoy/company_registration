e86a00662697d8bfcb7c74a6c956310a8e645cc2
Leonardo Cunha <leonardo.msc42@gmail.com>
2020-12-05 20:20:21 -0300

    feat:store method created for Company controller

--- app/Http/Controllers/CompanyCRUDController.php
+++ app/Http/Controllers/CompanyCRUDController.php
@@ -17,4 +17,20 @@ class CompanyCRUDController extends Controller
     {
         return view('companies.create');
     }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'address' => 'required'
+        ]);
+        $company = new Company;
+        $company->name = $request->name;
+        $company->email = $request->email;
+        $company->address = $request->address;
+        $company->save();
+        return redirect()->route('companies.index')
+        ->with('success', 'Company has been created successfully');
+    }
 }
